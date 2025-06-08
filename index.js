11540ae853ffd1549bdec7aaee19eafaf0c4465f
Emma24968 <emnabs12@gmail.com>
2025-06-08 17:05:38 +0100

    I added a click event listner to my button element and created a variable to hold input from my input field

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let myInput = document.getElementById('new-task-input');
 let mybtn =  document.getElementById('my-button');
 let mytask =  document.getElementById('task-list');
+
+mybtn.addEventListener('click', function() {
+    let taskText = myInput.value.trim();
+})
