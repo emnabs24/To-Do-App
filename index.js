73d1f4df13c7ffc9ab61a39303854e39e4ea4ad4
Emma24968 <emnabs12@gmail.com>
2025-06-08 17:16:56 +0100

    I created new elements for my list items,checkboxes and span

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ let mytask =  document.getElementById('task-list');
 
 mybtn.addEventListener('click', function() {
     let taskText = myInput.value.trim();
+    if (taskText === '')return
+
+    let li = document.createElement('li');
+
+    let checkbox = document.createElement('checkbox');
+    checkbox.type = 'checkbox';
+
+    let span = document.createElement('span');
+    span.textContent = taskText;
+
+    
+
 })
